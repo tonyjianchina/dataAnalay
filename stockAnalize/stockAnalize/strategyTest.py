c7392e45ebb38063926defa35b005c08ae7765e6
tonyjianchina <tonyjianchina@gmail.com>
2016-09-14 22:22:06 +0800

    新增计算最大回撤方法

--- stockAnalize/stockAnalize/strategyTest.py
+++ stockAnalize/stockAnalize/strategyTest.py
@@ -21,4 +21,33 @@ def annual_return(capital_line,date_line):
     annual=pow(df.ix[len(df.index)-1,'capital']/df.ix[0,'capital'],250/len(rng)-1)
     print '年化收益率：%s' %annual
     
-annual_return(capital_line1,date_line1)
+# annual_return(capital_line1,date_line1)
+
+# 计算最大回撤
+
+def max_draw_back(date_line,capital_line):
+    new_frame = pd.DataFrame({'date':date_line,'capital':capital_line})
+    new_frame.sort_values(by='date',inplace=True)
+    new_frame.reset_index(drop=True,inplace=True)
+
+    new_frame['max2here']=pd.expanding_max(new_frame['capital']) #计算该日之前的最大受益
+    new_frame['todayDrawBack']=new_frame['capital']/new_frame['max2here']-1
+
+    # 计算最大回撤和结束时间
+    temp=new_frame.sort_values(by='todayDrawBack').iloc[0][['date','todayDrawBack']]
+    max_dd = temp['todayDrawBack']
+    end_date=temp['date']
+
+
+    # 计算开始时间
+    pre_date = new_frame[new_frame['date']<=end_date]
+    start_date = pre_date.sort_values(by='todayDrawBack',ascending=False).iloc[0]['date']
+
+    print '最大回撤为%f,回撤开始时间为%s,回撤结束时间为%s' % (max_dd,start_date,end_date)
+
+    # 画图
+    new_frame.set_index('date',inplace=True)
+    new_frame.plot()
+    plt.show()
+
+max_draw_back(date_line1,capital_line1)
